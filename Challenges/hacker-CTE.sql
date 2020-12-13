745a4dd34fa79b260f9a4b554a399ca8b7db8088
nish4596 <68756896+nish4596@users.noreply.github.com>
2020-12-13 01:10:50 -0700

    Create hacker-CTE.sql

---
+++ Challenges/hacker-CTE.sql
@@ -0,0 +1,9 @@
+Julia asked her students to create some coding challenges. Write a query to print the hacker_id, name, and the total number of challenges created by each student. Sort your results by the total number of challenges in descending order. If more than one student created the same number of challenges, then sort the result by hacker_id. If more than one student created the same number of challenges and the count is less than the maximum number of challenges created, then exclude those students from the result.
+
+Input Format
+
+The following tables contain challenge data:
+
+Hackers: The hacker_id is the id of the hacker, and name is the name of the hacker.
+
+Challenges: The challenge_id is the id of the challenge, and hacker_id is the id of the student who created the challenge.
